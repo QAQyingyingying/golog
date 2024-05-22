55bf7cade0a55d2191251ad93f09b9d45a2ae272
QAQ <Mystic.wl@outlook.com>
2024-05-22 20:40:59 +0800

    添加注释,闲得没事

--- color.go
+++ color.go
@@ -13,36 +13,50 @@ const (
 	textWhite
 )
 
+// Black 黑色信息
 func Black(msg ...any) string {
 	str := fmt.Sprint(msg...)
 	return textColor(textBlack, str)
 }
 
+// Red 红色信息
 func Red(msg ...any) string {
 	str := fmt.Sprint(msg...)
 	return textColor(textRed, str)
 }
+
+// Yellow 黄色信息
 func Yellow(msg ...any) string {
 	str := fmt.Sprint(msg...)
 	return textColor(textYellow, str)
 }
+
+// Green 绿色信息
 func Green(msg ...any) string {
 	str := fmt.Sprint(msg...)
 	return textColor(textGreen, str)
 }
+
+// Cyan 青色信息
 func Cyan(msg ...any) string {
 	str := fmt.Sprint(msg...)
 	return textColor(textCyan, str)
 }
+
+// Blue 蓝色信息
 func Blue(msg ...any) string {
 	str := fmt.Sprint(msg...)
 	return textColor(textBlue, str)
 }
+
+// Purple 紫色信息
 func Purple(msg ...any) string {
 	str := fmt.Sprint(msg...)
 	return textColor(textPurple, str)
 }
-func white(msg ...any) string {
+
+// White 白色信息
+func White(msg ...any) string {
 	str := fmt.Sprint(msg...)
 	return textColor(textWhite, str)
 }
--- golog.go
+++ golog.go
@@ -5,6 +5,7 @@ import (
 	"runtime"
 )
 
+// Info 普通信息
 func Info(msg ...any) {
 	pc, _, _, ok := runtime.Caller(1)
 	funcName := "unKnow"
@@ -14,6 +15,8 @@ func Info(msg ...any) {
 	log.SetPrefix("INFO  " + "[" + funcName + "] ")
 	log.Print(white(msg...))
 }
+
+// Debug 调试信息
 func Debug(msg ...any) {
 	pc, _, _, ok := runtime.Caller(1)
 	funcName := "unKnow"
@@ -23,6 +26,8 @@ func Debug(msg ...any) {
 	log.SetPrefix(Green("DEBUG " + "[" + funcName + "] "))
 	log.Print(Green(msg...))
 }
+
+// Warn 警告信息
 func Warn(msg ...any) {
 	pc, _, _, ok := runtime.Caller(1)
 	funcName := "unKnow"
@@ -32,6 +37,8 @@ func Warn(msg ...any) {
 	log.SetPrefix(Yellow("WARN  " + "[" + funcName + "] "))
 	log.Print(Yellow(msg...))
 }
+
+// Error 错误信息
 func Error(msg ...any) {
 	pc, _, _, ok := runtime.Caller(1)
 	funcName := "unKnow"
@@ -41,6 +48,8 @@ func Error(msg ...any) {
 	log.SetPrefix(Red("ERROR " + "[" + funcName + "] "))
 	log.Print(Red(msg...))
 }
+
+// ErrorAndExit 错误信息并退出程序
 func ErrorAndExit(msg ...any) {
 	pc, _, _, ok := runtime.Caller(1)
 	funcName := "unKnow"
